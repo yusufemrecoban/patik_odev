41ec49d4001667d5b99734eafe61608e150e9672
yusuf <yusuffemreecobann@gmail.com>
2023-07-29 01:02:56 +0300

    patika ödev

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace patik_odev{
+    class Program{
+        static void Main(string[] args){
+           Console.WriteLine("kaç tane sayı girmek istiyorsunuz: ");
+              int sayi = Convert.ToInt32(Console.ReadLine());
+              int[] sayilar = new int[sayi];
+                for(int i = 0; i < sayi; i++)
+                {
+                    Console.WriteLine("sayı giriniz: ");
+                    sayilar[i] = Convert.ToInt32(Console.ReadLine());
+                }
+                foreach( int sayim in sayilar)
+                {
+                    if( sayim%2==0)
+                    {
+                        Console.WriteLine("çift sayılar");
+                        Console.WriteLine(sayim);
+                    }
+                }
+           Console.ReadLine();
+        }
+    }
+}
